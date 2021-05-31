40ac3632fc737d1c1255f6c9aca21d821eee3162
Prochg <Prochg@users.noreply.github.com>
2021-05-31 04:51:38 +0000

    deploy: a440984638223f470fa06987aac23f12a7d3f5b4

---
+++ index_props.js
@@ -0,0 +1,41 @@
+import projectConfig from '/docs/pagic.config.js';
+export default {
+    'prev': undefined,
+    'next': undefined,
+    config: { "root": "/", ...projectConfig, branch: 'master' },
+    'pagePath': "README.md",
+    'layoutPath': "_layout.tsx",
+    'outputPath': "index.html",
+    'title': undefined,
+    'content': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<p><a href="https://prochg.github.io/docs/">斜阳草树 寻常巷陌</a></p>'
+        } }),
+    'head': null,
+    'script': React.createElement(React.Fragment, null,
+        React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
+        React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
+        React.createElement("script", { src: "/docs/index.js", type: "module" })),
+    'contentTitle': undefined,
+    'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<p><a href="https://prochg.github.io/docs/">斜阳草树 寻常巷陌</a></p>'
+        } }),
+    'toc': null,
+    'author': "yvanlin",
+    'contributors': [
+        "yvanlin"
+    ],
+    'date': "2021-05-31T04:42:14.000Z",
+    'updated': null,
+    'excerpt': "斜阳草树 寻常巷陌",
+    'cover': undefined,
+    'sidebar': [
+        {
+            "text": "C++",
+            "children": []
+        },
+        {
+            "text": "Windows系统开发",
+            "children": []
+        }
+    ]
+};
---
+++ pagic.config.js
@@ -0,0 +1,34 @@
+const nav = [
+    {
+        text: '技术文档',
+        link: '/docs/index.html',
+    },
+];
+const sidebar = {
+    '/': [
+        {
+            text: 'C++',
+            children: []
+        },
+        {
+            text: 'Windows系统开发',
+            children: [],
+        },
+    ],
+};
+export default {
+    srcDir: 'docs',
+    root: '/docs/',
+    theme: 'docs',
+    plugins: ['sidebar', 'prev_next'],
+    title: '斜阳草树 寻常巷陌',
+    description: '',
+    github: 'https://github.com/Prochg/docs',
+    nav,
+    sidebar,
+    tools: {
+        editOnGitHub: false,
+        backToTop: true,
+    },
+    port: 8000,
+};
